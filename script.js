f9eb8a313f63de804dfd28e72ea62732425d0397
Viktor Sharha <vsharha07@gmail.com>
2024-08-31 00:31:42 +0100

    various changes to design, added portfolio project dates, added article text

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const style = getComputedStyle(document.body);
-const sections = document.querySelectorAll("section");
+// const style = getComputedStyle(document.body);
+// const sections = document.querySelectorAll("section");
 
-function getCSSvar(varName) {
-	return style.getPropertyValue("--" + varName);
-}
+// function getCSSvar(varName) {
+// 	return style.getPropertyValue("--" + varName);
+// }
 
-function onReady() {
-	for (let i = 0; i < sections.length; i++) {
-		if (i % 2 == 1) {
-			sections[i].style.background = getCSSvar("alt-primary");
-		}
-	}
-}
+// function onReady() {
+// 	for (let i = 0; i < sections.length; i++) {
+// 		if (i % 2 == 1) {
+// 			sections[i].style.background = getCSSvar("alt-primary");
+// 		}
+// 	}
+// }
 
-window.onload = onReady();
+// window.onload = onReady();
